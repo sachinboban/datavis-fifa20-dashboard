9496f1a3a00ad34fb1a5c2f00094448446ca0b6a
Sachin Boban <sachinbobank@gmail.com>
2019-11-23 15:35:32 -0700

    Add BarChart
    Add BarChart component.
    Use BarChart to display Overall in the table.
    
    Signed-off-by: Sachin Boban <sachinbobank@gmail.com>

---
+++ src/barChart.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+function getBarChartJSX(width, maxWidth) {
+  const actualWidth = (width * maxWidth) / 100;
+  const barStyle = {
+    width: actualWidth
+  };
+
+  return (
+    <div className="bar-chart">
+      <div className="bar" style={barStyle}>
+        {width}
+      </div>
+    </div>
+  );
+}
+
+export default getBarChartJSX;
--- src/table.js
+++ src/table.js
@@ -1,100 +1,110 @@
-import React from 'react';
-import Col from 'react-bootstrap/Col';
-import {Paper} from "@material-ui/core";
+import React from "react";
+import Col from "react-bootstrap/Col";
+import { Paper } from "@material-ui/core";
 import {
-    Grid,
-    VirtualTable,
-    TableHeaderRow,
-    TableFilterRow,
-    TableSelection,
-    TableGroupRow,
-    GroupingPanel,
-    DragDropProvider,
-    Toolbar,
+  Grid,
+  VirtualTable,
+  TableHeaderRow,
+  TableFilterRow,
+  TableSelection,
+  TableGroupRow,
+  GroupingPanel,
+  DragDropProvider,
+  Toolbar
 } from "@devexpress/dx-react-grid-material-ui";
 import {
-    FilteringState,
-    SortingState,
-    IntegratedFiltering,
-    IntegratedSorting,
-    SelectionState,
-    GroupingState,
-    IntegratedGrouping,
-} from '@devexpress/dx-react-grid';
+  FilteringState,
+  SortingState,
+  IntegratedFiltering,
+  IntegratedSorting,
+  SelectionState,
+  GroupingState,
+  IntegratedGrouping
+} from "@devexpress/dx-react-grid";
+import BarChart from "./barChart";
 
 class PlayerTable extends React.Component {
-    constructor(props) {
-        super(props);
-        this.columnHeaders = ["Name", "Country", "Club", "League", "Position", "Overall"];
-        this.columnWidths = [{ columnName: "Name", width: 200},{ columnName: "Country", width: 100},{ columnName: "Club", width: 150},
-            { columnName: "League", width: 150}, { columnName: "Position", width: 100}, { columnName: "Overall", width: 100}
-        ];
-        this.state = {
-            rows: [],
-            columns: [],
-            selection: []
-        }
-    }
-
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        if (prevProps.data !== this.props.data) {
-            let rows = this.props.data.slice();
-            let columns = this.columnHeaders.map(col => {
-                return {name: col, title: col}
-            });
-            this.setState({
-                rows: rows,
-                columns: columns,
-                selection: []
-            });
-        }
-    }
-
-    onSelectionChange = (selection) => {
-        this.setState({
-            rows: this.state.rows,
-            columns: this.state.columns,
-            selection: selection
-        });
-        this.props.onSelectionChange(this.state.rows[selection[selection.length - 1]]);
+  constructor(props) {
+    super(props);
+    this.columnHeaders = [
+      "Name",
+      "Country",
+      "Club",
+      "League",
+      "Position",
+      "Overall"
+    ];
+    this.columnWidths = [
+      { columnName: "Name", width: 200 },
+      { columnName: "Country", width: 100 },
+      { columnName: "Club", width: 150 },
+      { columnName: "League", width: 150 },
+      { columnName: "Position", width: 100 },
+      { columnName: "Overall", width: 100 }
+    ];
+    this.state = {
+      rows: [],
+      columns: [],
+      selection: []
     };
+  }
 
-
-    render() {
-        return (
-            <Col>
-                <Paper>
-                    <Grid
-                        rows={this.state.rows}
-                        columns={this.state.columns}
-                    >
-                        <SortingState
-                            defaultSorting={[{columnName: 'Name', direction: 'asc'}]}
-                        />
-                        <IntegratedSorting/>
-
-                        <DragDropProvider/>
-                        <GroupingState
-                            defaultGrouping={[]}/>
-                        <IntegratedGrouping/>
-                        <FilteringState defaultFilters={[]}/>
-                        <IntegratedFiltering/>
-                        <SelectionState
-                            selection={this.state.selection}
-                            onSelectionChange={this.onSelectionChange}
-                        />
-                        <VirtualTable  columnExtensions={this.columnWidths}/>
-                        <TableHeaderRow showSortingControls/>
-                        <TableSelection selectByRowClick highlightRow/>
-                        <TableGroupRow/>
-                        <Toolbar/>
-                        <GroupingPanel showGroupingControls showSortingControls/>
-                        <TableFilterRow/>
-                    </Grid>
-                </Paper>
-            </Col>
-        );
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if (prevProps.data !== this.props.data) {
+      let rows = this.props.data.slice();
+      let columns = this.columnHeaders.map(col => {
+        return { name: col, title: col };
+      });
+      this.setState({
+        rows: rows,
+        columns: columns,
+        selection: []
+      });
     }
+  }
+
+  onSelectionChange = selection => {
+    this.setState({
+      rows: this.state.rows,
+      columns: this.state.columns,
+      selection: selection
+    });
+    this.props.onSelectionChange(
+      this.state.rows[selection[selection.length - 1]]
+    );
+  };
+
+  render() {
+    return (
+      <Col>
+        <Paper>
+          <Grid rows={this.state.rows} columns={this.state.columns}>
+            <SortingState
+              defaultSorting={[{ columnName: "Name", direction: "asc" }]}
+            />
+            <IntegratedSorting />
+
+            <DragDropProvider />
+            <GroupingState defaultGrouping={[]} />
+            <IntegratedGrouping />
+            <FilteringState defaultFilters={[]} />
+            <IntegratedFiltering />
+            <SelectionState
+              selection={this.state.selection}
+              onSelectionChange={this.onSelectionChange}
+            />
+            <VirtualTable columnExtensions={this.columnWidths} />
+            <TableHeaderRow showSortingControls />
+            <TableSelection selectByRowClick highlightRow />
+            <TableGroupRow />
+            <Toolbar />
+            <GroupingPanel showGroupingControls showSortingControls />
+            <TableFilterRow />
+          </Grid>
+        </Paper>
+      </Col>
+    );
+  }
 }
 
 export default PlayerTable;
