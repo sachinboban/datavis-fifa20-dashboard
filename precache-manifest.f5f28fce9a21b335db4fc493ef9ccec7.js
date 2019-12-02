9be7c99036d4a3fc3c252819ee890469b35bae75
Lakshmi Narayanan <lakshminarayanan.ramasamy@utah.edu>
2019-12-01 20:16:41 -0700

    Updates

---
+++ precache-manifest.f5f28fce9a21b335db4fc493ef9ccec7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4c96d1fca2e944479dd8fd9e18a7c5",
+    "url": "/fifa20-dashboard/index.html"
+  },
+  {
+    "revision": "1859e62e516a836c0955",
+    "url": "/fifa20-dashboard/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "971beef3a86222154400",
+    "url": "/fifa20-dashboard/static/css/main.1b0ca3bc.chunk.css"
+  },
+  {
+    "revision": "1859e62e516a836c0955",
+    "url": "/fifa20-dashboard/static/js/2.b50e04d0.chunk.js"
+  },
+  {
+    "revision": "971beef3a86222154400",
+    "url": "/fifa20-dashboard/static/js/main.5fc4d65e.chunk.js"
+  },
+  {
+    "revision": "7148621863f38be87e49",
+    "url": "/fifa20-dashboard/static/js/runtime-main.8fe15821.js"
+  },
+  {
+    "revision": "8671d7e588aacd8e764e984aca09013e",
+    "url": "/fifa20-dashboard/static/media/both.8671d7e5.png"
+  },
+  {
+    "revision": "d1c8b8c8cf9f9a57a926912f14aafdad",
+    "url": "/fifa20-dashboard/static/media/fifa-logo.d1c8b8c8.png"
+  },
+  {
+    "revision": "d3335a9d67b71802f82b04483c315744",
+    "url": "/fifa20-dashboard/static/media/left.d3335a9d.png"
+  },
+  {
+    "revision": "8f530e4640a7a9fecec5aa6b831e0ca8",
+    "url": "/fifa20-dashboard/static/media/raw.8f530e46.csv"
+  },
+  {
+    "revision": "6a6888d5ec3ba8c21cb143d782f361d0",
+    "url": "/fifa20-dashboard/static/media/right.6a6888d5.png"
+  },
+  {
+    "revision": "d0b88b1efb4fde96cb7e5f440d19e1bc",
+    "url": "/fifa20-dashboard/static/media/team-league.d0b88b1e.csv"
+  }
+]);
